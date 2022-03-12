f2858b9ef05c789695539ec4345a5e52985060a0
Felix Jones <felix@felixjones.co.uk>
2022-03-12 13:30:45 +0100

    Added development sample project

---
+++ samples/01 gba-hpp/main.cpp
@@ -0,0 +1,14 @@
+/*
+===============================================================================
+
+ Copyright (C) 2022 gba-hpp contributors
+ For conditions of distribution and use, see copyright notice in LICENSE.md
+
+===============================================================================
+*/
+
+#include <gba/gba.hpp>
+
+int main() {
+    return 0;
+}

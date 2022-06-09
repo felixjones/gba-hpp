c102ec22e0e8222c64d3ace80c39ced28b155055
Felix Jones <felix@felixjones.co.uk>
2022-06-09 21:17:03 +0200

    Added key_mask and key_state types for handling key input

--- include/gba/registers/key.hpp
+++ include/gba/registers/key.hpp
@@ -10,16 +10,74 @@
 #ifndef GBAXX_REGISTERS_KEY_HPP
 #define GBAXX_REGISTERS_KEY_HPP
 
+#include <bit>
+
 #include <gba/ioregister.hpp>
+#include <gba/inttype.hpp>
 
 #include <gba/registers/accessors.hpp>
 #include <gba/registers/key_types.hpp>
 
-namespace gba::reg {
+namespace gba {
+namespace reg {
 
-    using keyinput = read_only<key_type, 0x4000130>;
-    using keycnt = read_only<key_type, 0x4000130>;
+    using key = read_only<uint16, 0x4000130>;
+    using keycnt = read_only<keycnt_type, 0x4000132>;
 
-} // namespace gba::reg
+} // namespace reg
+
+class key_state {
+public:
+    auto& poll() noexcept {
+        m_previous = m_current;
+        m_current = reg::key::get();
+        return *this;
+    }
+
+    [[nodiscard]]
+    auto down(key_mask mask) const noexcept {
+        const auto bits = std::bit_cast<uint32>(mask);
+        return (m_current & bits) == 0;
+    }
+
+    [[nodiscard]]
+    auto up(key_mask mask) const noexcept {
+        const auto bits = std::bit_cast<uint32>(mask);
+        return (m_current & bits) == bits;
+    }
+
+    [[nodiscard]]
+    auto hit(key_mask mask) const noexcept {
+        const auto bits = std::bit_cast<uint32>(mask);
+        if ((m_previous & bits) == bits) {
+            return (m_current & bits) == 0;
+        }
+    }
+
+    [[nodiscard]]
+    auto released(key_mask mask) const noexcept {
+        const auto bits = std::bit_cast<uint32>(mask);
+        if ((m_previous & bits) == 0) {
+            return (m_current & bits) == bits;
+        }
+    }
+
+    [[nodiscard]]
+    auto horizontal() const noexcept {
+        const auto sign = (static_cast<int>(~m_current) << 26) >> 30;
+        return sign | (sign >> 1); // Fixup low bit
+    }
+
+    [[nodiscard]]
+    auto vertical() const noexcept {
+        const auto sign = (static_cast<int>(~m_current) << 24) >> 30;
+        return sign | (sign >> 1); // Fixup low bit
+    }
+private:
+    uint16 m_current{0xffff};
+    uint16 m_previous{0xffff};
+};
+
+} // namespace gba
 
 #endif // define GBAXX_REGISTERS_KEY_HPP
--- include/gba/registers/key_types.hpp
+++ include/gba/registers/key_types.hpp
@@ -17,34 +17,6 @@
 
 namespace gba {
 
-struct alignas(uint16) key_type {
-    bool a : 1;
-    bool b : 1;
-    bool select : 1;
-    bool start : 1;
-    bool right : 1;
-    bool left : 1;
-    bool up : 1;
-    bool down : 1;
-    bool r : 1;
-    bool l : 1;
-};
-
-namespace key {
-
-    static constexpr auto a = field_of::boolean<key_type, 0>();
-    static constexpr auto b = field_of::boolean<key_type, 1>();
-    static constexpr auto select = field_of::boolean<key_type, 2>();
-    static constexpr auto start = field_of::boolean<key_type, 3>();
-    static constexpr auto right = field_of::boolean<key_type, 4>();
-    static constexpr auto left = field_of::boolean<key_type, 5>();
-    static constexpr auto up = field_of::boolean<key_type, 6>();
-    static constexpr auto down = field_of::boolean<key_type, 7>();
-    static constexpr auto r = field_of::boolean<key_type, 8>();
-    static constexpr auto l = field_of::boolean<key_type, 9>();
-
-} // namespace key
-
 enum class keycnt_condition : uint8 {
     _or,
     _and
@@ -84,7 +56,36 @@ namespace keycnt {
         return field_of::enum_class<keycnt_type, 15, keycnt_condition, keycnt_condition::_and>(i);
     }
 
+} // namespace keycnt
+
+struct alignas(uint32) key_mask {
+    bool a : 1;
+    bool b : 1;
+    bool select : 1;
+    bool start : 1;
+    bool right : 1;
+    bool left : 1;
+    bool up : 1;
+    bool down : 1;
+    bool r : 1;
+    bool l : 1;
+};
+
+namespace key {
+
+    static constexpr auto a = field_of::boolean<key_mask, 0>();
+    static constexpr auto b = field_of::boolean<key_mask, 1>();
+    static constexpr auto select = field_of::boolean<key_mask, 2>();
+    static constexpr auto start = field_of::boolean<key_mask, 3>();
+    static constexpr auto right = field_of::boolean<key_mask, 4>();
+    static constexpr auto left = field_of::boolean<key_mask, 5>();
+    static constexpr auto up = field_of::boolean<key_mask, 6>();
+    static constexpr auto down = field_of::boolean<key_mask, 7>();
+    static constexpr auto r = field_of::boolean<key_mask, 8>();
+    static constexpr auto l = field_of::boolean<key_mask, 9>();
+
 } // namespace key
+
 } // namespace gba
 
 #endif // define GBAXX_REGISTERS_KEY_TYPES_HPP

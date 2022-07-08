5447a9a6357e4d5538d5bcbb67ffae40a753f8ec
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2022-07-09 01:55:28 +0200

    Fix wrong buffer size on Mode 5

--- include/gba/video/modes.hpp
+++ include/gba/video/modes.hpp
@@ -28,7 +28,7 @@ concept IsPixelArray = util::Array<T> &&
     (Mode == 4 && BinaryDigits<util::array_value_type<T>, 8>));
 
 template <class T, std::size_t Mode>
-concept IsLine = IsPixelArray<T, Mode> && (util::array_size<T> % 240) == 0;
+concept IsLine = IsPixelArray<T, Mode> && (util::array_size<T> % (Mode == 5 ? 128 : 240)) == 0;
 
 template <std::size_t Mode>
 struct mode;
@@ -69,7 +69,7 @@ struct mode<3> {
         __aeabi_memcpy4(reinterpret_cast<uint16*>(address) + (y * 240), &line[0], util::array_size<line_type> * sizeof(uint16));
 #else
         auto* src = reinterpret_cast<const uint32*>(&line[0]);
-        auto* dst= reinterpret_cast<volatile uint32*>(address);
+        auto* dst = reinterpret_cast<volatile uint32*>(address);
         dst += y * 120;
 
         for (int ii = 0; ii < util::array_size<line_type> / 2; ++ii) {
@@ -143,7 +143,7 @@ struct mode<4> {
         __aeabi_memcpy4(reinterpret_cast<uint8*>(addr) + (y * 240), &line[0], util::array_size<line_type>);
 #else
         auto* src = reinterpret_cast<const uint32*>(&line[0]);
-        auto* dst= reinterpret_cast<volatile uint32*>(addr);
+        auto* dst = reinterpret_cast<volatile uint32*>(addr);
         dst += y * 60;
 
         for (int ii = 0; ii < util::array_size<line_type> / 4; ++ii) {
@@ -161,9 +161,9 @@ struct mode<5> {
     static void clear(BinaryDigits<16> auto c, std::size_t frame) noexcept {
         const auto addr = address + (offset_frame * frame);
 #if defined(GBAXX_HAS_AGBABI)
-        __agbabi_wordset4(reinterpret_cast<uint16*>(addr), 240 * 160 * sizeof(uint16), ((c << 16) | c));
+        __agbabi_wordset4(reinterpret_cast<uint16*>(addr), 160 * 128 * sizeof(uint16), ((c << 16) | c));
 #else
-        constexpr auto end = (240 * 160) / 2;
+        constexpr auto end = (160 * 128) / 2;
         auto* buffer = reinterpret_cast<volatile uint32*>(addr);
         const auto cc = uint32((c << 16) | c);
         for (int ii = 0; ii < end; ++ii) {
@@ -177,13 +177,13 @@ struct mode<5> {
         using int16_type = decltype(c);
 
         auto* buffer = reinterpret_cast<volatile int16_type*>(addr);
-        buffer[(y * 240) + x] = c;
+        buffer[(y * 160) + x] = c;
     }
 
     static auto get(int x, int y, std::size_t frame) noexcept {
         const auto addr = address + (offset_frame * frame);
         auto* buffer = reinterpret_cast<const volatile uint16*>(addr);
-        return buffer[(y * 240) + x];
+        return buffer[(y * 160) + x];
     }
 
     static void put_row(int y, const IsLine<3> auto& line, std::size_t frame) noexcept {
@@ -191,11 +191,11 @@ struct mode<5> {
 
         const auto addr = address + (offset_frame * frame);
 #if defined(GBAXX_HAS_AGBABI)
-        __aeabi_memcpy4(reinterpret_cast<uint16*>(addr) + (y * 240), &line[0], util::array_size<line_type> * sizeof(uint16));
+        __aeabi_memcpy4(reinterpret_cast<uint16*>(addr) + (y * 160), &line[0], util::array_size<line_type> * sizeof(uint16));
 #else
         auto* src = reinterpret_cast<const uint32*>(&line[0]);
-        auto* dst= reinterpret_cast<volatile uint32*>(addr);
-        dst += y * 120;
+        auto* dst = reinterpret_cast<volatile uint32*>(addr);
+        dst += y * 80;
 
         for (int ii = 0; ii < util::array_size<line_type> / 2; ++ii) {
             dst[ii] = src[ii];
@@ -206,13 +206,13 @@ struct mode<5> {
     static void put_row(int x, int y, const IsPixelArray<3> auto& line, std::size_t frame) noexcept {
         const auto addr = address + (offset_frame * frame);
 #if defined(GBAXX_HAS_AGBABI)
-        __agbabi_memcpy2(reinterpret_cast<uint16*>(addr) + (y * 240) + x, &line[0], sizeof(line));
+        __agbabi_memcpy2(reinterpret_cast<uint16*>(addr) + (y * 160) + x, &line[0], sizeof(line));
 #else
         using line_type = std::remove_reference_t<decltype(line)>;
         using int16_type = util::array_value_type<line_type>;
 
         auto* buffer = reinterpret_cast<volatile int16_type*>(addr);
-        buffer += (y * 240) + x;
+        buffer += (y * 160) + x;
         for (std::size_t ii = 0; ii < util::array_size<line_type>; ++ii) {
             buffer[ii] = line[ii];
         }

1b10bca7bedbd70a0153a310100a7e764e2e17a1
Felix Jones <felix@felixjones.co.uk>
2023-07-20 22:59:28 +0200

    BIOS functions up to sound stuff

--- include/gba/bios/math.hpp
+++ include/gba/bios/math.hpp
@@ -20,23 +20,23 @@ namespace {
 
     [[nodiscard, gnu::always_inline, gnu::const]]
     inline auto Div(int number, int denom) noexcept {
-        register int r0 asm("r0") = number;
-        register int r1 asm("r1") = denom;
+        register auto r0 asm("r0") = number;
+        register auto r1 asm("r1") = denom;
         asm volatile inline ("swi 0x6 << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) :: "r3");
         return make_vector<int, 2>{r0, r1};
     }
 
     [[nodiscard, gnu::always_inline, gnu::const]]
     inline auto DivArm(int denom, int number) noexcept {
-        register int r0 asm("r0") = denom;
-        register int r1 asm("r1") = number;
-        asm volatile inline ("swi 0x7 << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) :: "r3");
+        register auto r0 asm("r0") = denom;
+        register auto r1 asm("r1") = number;
+        asm volatile inline ("swi 0x7 << ((1f - . == 4) * -16); 1:" : "+r"(r0) : "r"(r1) : "r3");
         return make_vector<int, 2>{r0, r1};
     }
 
     [[nodiscard, gnu::always_inline, gnu::const]]
     inline auto Sqrt(u32 arg) noexcept {
-        register u32 r0 asm("r0") = arg;
+        register auto r0 asm("r0") = arg;
         asm volatile inline ("swi 0x8 << ((1f - . == 4) * -16); 1:" : "+r"(r0) :: "r1", "r3");
         return r0;
     }
@@ -52,7 +52,7 @@ namespace {
         static constexpr auto half_exp = (fixed_type::fractional_bits + 1) / 2;
         static constexpr auto odd_bit = fixed_type::fractional_bits % 2;
 
-        register unsigned_type r0 asm("r0") = unsigned_type(arg.data() << ((Exp * 2) + odd_bit));
+        register auto r0 asm("r0") = unsigned_type(arg.data() << ((Exp * 2) + odd_bit));
         asm volatile inline ("swi 0x8 << ((1f - . == 4) * -16); 1:" : "+r"(r0) :: "r1", "r3");
         return fixed<unsigned_type, half_exp + Exp>::from_data(r0);
     }
@@ -68,8 +68,8 @@ namespace {
     inline auto ArcTan2(fixed<int, 14> x, fixed<int, 14> y) noexcept {
         register auto r0 asm("r0") = x.data();
         register auto r1 asm("r1") = y.data();
-        asm volatile inline ("swi 0xA << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) :: "r3");
-        return angle<int, 16>{x.data()};
+        asm volatile inline ("swi 0xA << ((1f - . == 4) * -16); 1:" : "+r"(r0) : "r"(r1) : "r3");
+        return angle<int, 16>{r0};
     }
 
     struct bg_affine_src {
@@ -93,11 +93,11 @@ namespace {
     };
 
     [[gnu::always_inline, gnu::nonnull(1, 2)]]
-    inline void BgAffineSet(const bg_affine_src* src, volatile bg_affine_dest* dest, std::size_t num) noexcept {
-        register auto r0 asm("r0") = src;
-        register auto r1 asm("r1") = dest;
+    inline void BgAffineSet(const bg_affine_src* __restrict__ src, volatile bg_affine_dest* __restrict__ dest, std::size_t num) noexcept {
+        register auto* r0 asm("r0") = src;
+        register auto* r1 asm("r1") = dest;
         register auto r2 asm("r2") = num;
-        asm volatile inline ("swi 0xE << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1), "=r"(r2) :: "r3");
+        asm volatile inline ("swi 0xE << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) : "r"(r2) : "r3", "memory");
     }
 
     struct obj_affine_src {
@@ -108,12 +108,12 @@ namespace {
     };
 
     [[gnu::always_inline, gnu::nonnull(1, 2)]]
-    inline void ObjAffineSet(const obj_affine_src* src, volatile fixed<short, 8>* dest, std::size_t num, std::size_t stride) noexcept {
-        register auto r0 asm("r0") = src;
-        register auto r1 asm("r1") = dest;
+    inline void ObjAffineSet(const obj_affine_src* __restrict__ src, volatile fixed<short, 8>* __restrict__ dest, std::size_t num, std::size_t stride) noexcept {
+        register auto* r0 asm("r0") = src;
+        register auto* r1 asm("r1") = dest;
         register auto r2 asm("r2") = num;
         register auto r3 asm("r3") = stride;
-        asm volatile inline ("swi 0xF << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1), "=r"(r2), "+r"(r3));
+        asm volatile inline ("swi 0xF << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1), "+r"(r3) : "r"(r2) : "memory");
     }
 
     [[nodiscard, gnu::always_inline, gnu::const]]
--- include/gba/bios/misc.hpp
+++ include/gba/bios/misc.hpp
@@ -10,15 +10,88 @@
 #ifndef GBAXX_BIOS_MISC_HPP
 #define GBAXX_BIOS_MISC_HPP
 
+#include <gba/type.hpp>
+#include <gba/hardware/serial.hpp>
+
 namespace gba::bios {
 
 namespace {
 
-    [[gnu::always_inline, gnu::const]]
+    [[nodiscard, gnu::always_inline, gnu::const]]
     inline int GetBiosChecksum() noexcept {
         register int i asm("r0");
         asm volatile inline ("swi 0xD << ((1f - . == 4) * -16); 1:" : "=r"(i) :: "r1", "r3");
         return i;
+    }
+
+    struct alignas(int) cpu_fast_set {
+        u32 count : 21{};
+        int : 3;
+        bool fill : 1{};
+    };
+
+    [[gnu::always_inline, gnu::nonnull(1, 2)]]
+    inline void CpuFastSet(const void* __restrict__ src, volatile void* __restrict__ dest, cpu_fast_set lenmode) noexcept {
+        register auto* r0 asm("r0") = src;
+        register auto* r1 asm("r1") = dest;
+        register auto r2 asm("r2") = lenmode;
+        asm volatile inline ("swi 0xC << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) : "r"(r2) : "r3", "memory");
+    }
+
+    struct alignas(int) cpu_set {
+        u32 count : 21{};
+        int : 3;
+        bool fill : 1{};
+        int : 1;
+        bool set_32bit : 1{};
+    };
+
+    [[gnu::always_inline, gnu::nonnull(1, 2)]]
+    inline void CpuSet(const void* __restrict__ src, volatile void* __restrict__ dest, cpu_set lenmode) noexcept {
+        register auto* r0 asm("r0") = src;
+        register auto* r1 asm("r1") = dest;
+        register auto r2 asm("r2") = lenmode;
+        asm volatile inline ("swi 0xB << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) : "r"(r2) : "r3", "memory");
+    }
+
+    [[noreturn, gnu::always_inline]]
+    inline void SoftReset() noexcept {
+        asm volatile inline ("swi 0x0 << ((1f - . == 4) * -16); 1:");
+        __builtin_unreachable();
+    }
+
+    struct alignas(int) ram_reset {
+        bool ewram : 1{};
+        bool iwram : 1{};
+        bool palette : 1{};
+        bool vram : 1{};
+        bool oam : 1{};
+        bool reg_sio : 1{};
+        bool reg_sound : 1{};
+        bool reg : 1{};
+    };
+
+    [[gnu::always_inline]]
+    inline void RegisterRamReset(ram_reset flags) noexcept {
+        register auto r0 asm("r0") = flags;
+        asm volatile inline ("swi 0x1 << ((1f - . == 4) * -16); 1:" : "+r"(r0) :: "r1", "r3", "memory");
+    }
+
+    [[noreturn, gnu::always_inline]]
+    inline void HardReset() noexcept {
+        asm volatile inline ("swi 0x26 << ((1f - . == 4) * -16); 1:");
+        __builtin_unreachable();
+    }
+
+    [[gnu::always_inline, gnu::nonnull(1)]]
+    inline auto MultiBoot(const multi_boot_param* param, multi_boot_mode mbmode) noexcept {
+        register union {
+            const multi_boot_param* ptr;
+            int res;
+        } r0 asm("r0") = {param};
+        register auto r1 asm("r1") = mbmode;
+        asm volatile inline ("swi 0x25 << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) :: "r3");
+        return r0.res;
     }
 
 }

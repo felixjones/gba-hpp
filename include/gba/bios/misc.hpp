f5d2a71f8aceb6bd7eb53fb38661697260b57132
Felix Jones <felix@felixjones.co.uk>
2023-07-30 22:52:28 +0200

    Documentation for bios

--- include/gba/bios/misc.hpp
+++ include/gba/bios/misc.hpp
@@ -17,6 +17,16 @@ namespace gba::bios {
 
 namespace {
 
+    /**
+     * @brief Retrieves the BIOS checksum.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-0dh-gba---getbioschecksum-undocumented">SWI 0Dh (GBA) - GetBiosChecksum (Undocumented)</a>
+     *
+     * The GBA is expected to return `0xBAAE187F`, whereas the DS (in GBA mode) is expected to return `0xBAAE1880`.
+     *
+     * @return The checksum value of the BIOS.
+     *
+     * @note This is an undocumented feature.
+     */
     [[nodiscard, gnu::always_inline, gnu::const]]
     inline int GetBiosChecksum() noexcept {
         register int i asm("r0");
@@ -24,12 +34,42 @@ namespace {
         return i;
     }
 
+    /**
+     * @brief Structure for CpuFastSet().
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-0ch-gbands7nds9dsi7dsi9---cpufastset">SWI 0Ch (GBA/NDS7/NDS9/DSi7/DSi9) - CpuFastSet</a>
+     *
+     * The fill flag is used to change CpuFastSet() from memcpy() behavior, to memset() behavior.
+     *
+     * @note The count is the number of words to copy (AKA: number of bytes divided by 4).
+     * @note For CpuFastSet() the `count` must be a multiple of 8 words.
+     *
+     * @sa CpuFastSet()
+     */
     struct alignas(int) cpu_fast_set {
         u32 count : 21{};
         int : 3;
         bool fill : 1{};
     };
 
+    /**
+     * @brief CpuFastSet function copies data from source to destination using CPU-specific optimizations.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-0ch-gbands7nds9dsi7dsi9---cpufastset">SWI 0Ch (GBA/NDS7/NDS9/DSi7/DSi9) - CpuFastSet</a>
+     *
+     * @param src Pointer to the source memory region.
+     * @param dest Pointer to the destination memory region.
+     * @param lenmode The length (multiple of 8) and mode (copy/fill) of the data to be copied.
+     *
+     * @note The lenmode::count must be a multiple of 8 words.
+     * @note When lenmode::fill is true this function performs a faster memset() than using DMA (Direct Memory Access)
+     *       registers.
+     * @note This function assumes that the provided source buffer and destination buffers are word aligned.
+     *
+     * @warning The function assumes that the src and dest buffers have sufficient space to store the data, and that
+     *          they do not overlap.
+     *
+     * @sa cpu_fast_set
+     * @sa CpuSet()
+     */
     [[gnu::always_inline, gnu::nonnull(1, 2)]]
     inline void CpuFastSet(const void* __restrict__ src, volatile void* __restrict__ dest, cpu_fast_set lenmode) noexcept {
         register auto* r0 asm("r0") = src;
@@ -38,6 +78,19 @@ namespace {
         asm volatile inline ("swi 0xC << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) : "r"(r2) : "r3", "memory");
     }
 
+    /**
+     * @brief Structure for CpuSet().
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-0bh-gbands7nds9dsi7dsi9---cpuset">SWI 0Bh (GBA/NDS7/NDS9/DSi7/DSi9) - CpuSet</a>
+     *
+     * The fill flag is used to change CpuSet() from memcpy() behavior, to memset() behavior.
+     * The set_32bit flag changes from 16-bit element size to 32-bit element size.
+     *
+     * @note The count is the number of elements to copy (AKA: number of bytes divided by 2 or 4, if set_32bit is set).
+     * @note For CpuSet() the `count` must be a multiple of 2 for 16-bit elements, or a multiple of 4 for 32-bit
+     *       elements (if set_32bit is set).
+     *
+     * @sa CpuSet()
+     */
     struct alignas(int) cpu_set {
         u32 count : 21{};
         int : 3;
@@ -46,6 +99,24 @@ namespace {
         bool set_32bit : 1{};
     };
 
+    /**
+     * @brief CpuSet function copies data from source to destination using CPU-specific optimizations.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-0bh-gbands7nds9dsi7dsi9---cpuset">SWI 0Bh (GBA/NDS7/NDS9/DSi7/DSi9) - CpuSet</a>
+     *
+     * @param src Pointer to the source memory region.
+     * @param dest Pointer to the destination memory region.
+     * @param lenmode The length (number of elements), mode (copy/fill), and size of elements for the data to be copied.
+     *
+     * @note The lenmode::count must be a multiple of the element size (when cpu_set::set_32bit is set).
+     * @note This function assumes that the provided source buffer and destination buffers are half-word aligned when
+     *       cpu_set::set_32bit is false, or word aligned when cpu_set::set_32bit is true.
+     *
+     * @warning The function assumes that the src and dest buffers have sufficient space to store the data, and that
+     *          they do not overlap.
+     *
+     * @sa cpu_set
+     * @sa CpuFastSet()
+     */
     [[gnu::always_inline, gnu::nonnull(1, 2)]]
     inline void CpuSet(const void* __restrict__ src, volatile void* __restrict__ dest, cpu_set lenmode) noexcept {
         register auto* r0 asm("r0") = src;
@@ -54,12 +125,37 @@ namespace {
         asm volatile inline ("swi 0xB << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1) : "r"(r2) : "r3", "memory");
     }
 
+    /**
+     * @brief Performs a soft reset.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-00h-gbands7nds9---softreset">SWI 00h (GBA/NDS7/NDS9) - SoftReset</a>
+     *
+     * This function triggers a software reset, also known as a soft reset, which restarts the program execution from
+     * the beginning.
+     *
+     * @note A flag stored in 0x3007FFA determines if the reset returns to ROM code, or EWRAM code.
+     * @note Clears 200h bytes of RAM (containing stacks, and BIOS IRQ vector/flags).
+     * @note Initializes system, supervisor, and irq stack pointers.
+     * @note Sets R0-R12, LR_svc, SPSR_svc, LR_irq, and SPSR_irq to zero.
+     * @note Enters system mode.
+     */
     [[noreturn, gnu::always_inline]]
     inline void SoftReset() noexcept {
         asm volatile inline ("swi 0x0 << ((1f - . == 4) * -16); 1:");
         __builtin_unreachable();
     }
 
+    /**
+     * @brief Represents the reset configuration for various sections of RAM.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-01h-gba---registerramreset">SWI 01h (GBA) - RegisterRamReset</a>
+     *
+     * This struct provides a convenient way to configure the reset behavior for different sections of RAM. Each member
+     * variable corresponds to a specific section of RAM and determines whether that section should be reset or not.
+     *
+     * @note The reg flag does not include reg_sio or reg_sound. All three reg flags must be set if you want to clear
+     *       all registers.
+     *
+     * @sa RegisterRamReset()
+     */
     struct alignas(int) ram_reset {
         bool ewram : 1{};
         bool iwram : 1{};
@@ -71,18 +167,52 @@ namespace {
         bool reg : 1{};
     };
 
+    /**
+     * \brief Resets the RAM based on the specified flags.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-01h-gba---registerramreset">SWI 01h (GBA) - RegisterRamReset</a>
+     *
+     * This function resets the RAM based on the flags passed as an argument.
+     * The RAM can be reset partially or fully depending on the flags.
+     *
+     * \param flags A bitmask specifying the RAM reset flags.
+     *
+     * \sa ram_reset
+     */
     [[gnu::always_inline]]
     inline void RegisterRamReset(ram_reset flags) noexcept {
         register auto r0 asm("r0") = flags;
         asm volatile inline ("swi 0x1 << ((1f - . == 4) * -16); 1:" : "+r"(r0) :: "r1", "r3", "memory");
     }
 
+    /**
+     * @brief Performs a hard reset of the system.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-26h-gba---hardreset-undocumented">SWI 26h (GBA) - HardReset (Undocumented)</a>
+     *
+     * The hard reset will completely shutdown and restart the system, restarting from the initial BIOS boot animation.
+     *
+     * @note This is an undocumented feature.
+     *
+     * @sa SoftReset()
+     */
     [[noreturn, gnu::always_inline]]
     inline void HardReset() noexcept {
         asm volatile inline ("swi 0x26 << ((1f - . == 4) * -16); 1:");
         __builtin_unreachable();
     }
 
+    /**
+     * @brief Begins the MultiBoot protocol to transfer and execute program code to connected GBA serial clients.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-25h-gba---multiboot">SWI 25h (GBA) - MultiBoot</a>
+     *
+     * @param param A pointer to the multi_boot_param structure.
+     * @param mbmode The multi_boot_mode specifying the desired transfer speed and behavior.
+     * @return 0 if MultiBoot succeeded, 1 if MultiBoot failed.
+     *
+     * @remarks The multi_boot_param structure should be properly initialized before passing it to this function.
+     *
+     * @see multi_boot_param
+     * @see multi_boot_mode
+     */
     [[gnu::always_inline, gnu::nonnull(1)]]
     inline auto MultiBoot(const multi_boot_param* param, multi_boot_mode mbmode) noexcept {
         register union {
--- include/gba/bios/sound.hpp
+++ include/gba/bios/sound.hpp
@@ -16,6 +16,28 @@ namespace gba::bios {
 
 namespace {
 
+    /**
+     * @brief Calculates the frequency of a MIDI key with fine adjustment.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-1fh-gba---midikey2freq">SWI 1Fh (GBA) - MidiKey2Freq</a>
+     *
+     * This function takes a pointer to a wave, a MIDI key number, and a fine adjustment value as inputs and calculates the corresponding frequency.
+     *
+     * @param wave A pointer to the wave.
+     * @param key The MIDI key number.
+     * @param fineadj The fine adjustment value.
+     * @return The frequency of the MIDI key with fine adjustment.
+     *
+     * @note This function assumes that the wave pointer is pointing to a valid WaveData object.
+     * @note This function is made famous by its use to read BIOS memory without triggering the BIOS protection
+     *
+     * @section Reading BIOS memory:
+     * @code{cpp}
+     * auto read_bios_byte(std::size_t idx) noexcept {
+     *     const auto a = gba::bios::MidiKey2Freq(reinterpret_cast&lt;const void*>(idx - 4), 180 - 12, 0) * 2;
+     *     return std::byte(a >> 24);
+     * }
+     * @endcode
+     */
     [[nodiscard, gnu::always_inline, gnu::const]]
     inline auto MidiKey2Freq(const void* wave, int key, int fineadj) noexcept {
         register union {
@@ -28,28 +50,64 @@ namespace {
         return r0.res;
     }
 
+    /**
+     * @brief Transitions the bias level for sound output.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-19h-gba-or-swi-08h-nds7dsi7---soundbias">SWI 19h (GBA) or SWI 08h (NDS7/DSi7) - SoundBias</a>
+     *
+     * Increments or decrements the current level of the SOUNDBIAS register (with short delays) until reaching the
+     * desired new level. The upper bits of the register are kept unchanged.
+     *
+     * @param bias The bias value to be set.
+     */
     [[gnu::always_inline]]
     inline void SoundBias(u16 bias) noexcept {
         register auto r0 asm("r0") = bias;
         asm volatile inline ("swi 0x19 << ((1f - . == 4) * -16); 1:" : "+r"(r0) :: "r1", "r3");
     }
 
+    /**
+     * @brief Clears all direct sound channels and stops the sound.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-1eh-gba---soundchannelclear">SWI 1Eh (GBA) - SoundChannelClear</a>
+     */
     [[gnu::always_inline]]
     inline void SoundChannelClear() noexcept {
         asm volatile inline ("swi 0x1E << ((1f - . == 4) * -16); 1:" ::: "r0", "r1", "r3", "memory");
     }
 
+    /**
+     * @brief Initializes the sound driver. Call this only once when the game starts up.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-1ah-gba---sounddriverinit">SWI 1Ah (GBA) - SoundDriverInit</a>
+     *
+     * @param sa Pointer to SoundArea structure for sound driver work area.
+     *
+     * @warning You cannot initialize this driver multiple times, even if separate work areas have been prepared.
+     */
     [[gnu::always_inline]]
     inline void SoundDriverInit(void* sa) noexcept {
         register auto* r0 asm("r0") = sa;
         asm volatile inline ("swi 0x1A << ((1f - . == 4) * -16); 1:" : "+r"(r0) :: "r1", "r3", "memory");
     }
 
+    /**
+     * @brief Main routine of the sound driver.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-1ch-gba---sounddrivermain">SWI 1Ch (GBA) - SoundDriverMain</a>
+     *
+     * Call every 1/60 of a second.
+     */
     [[gnu::always_inline]]
     inline void SoundDriverMain() noexcept {
         asm volatile inline ("swi 0x1C << ((1f - . == 4) * -16); 1:" ::: "r0", "r1", "r3", "memory");
     }
 
+    /**
+     * @brief The sound driver mode configuration.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-1bh-gba---sounddrivermode">SWI 1Bh (GBA) - SoundDriverMode</a>
+     *
+     * The sound_mode struct stores the settings for sound mode including reverb, number of virtual channels, volume,
+     * frequency, and number digital-to-analog bits (between 6 and 9 bits, default is 8).
+     *
+     * @sa SoundDriverMode()
+     */
     struct alignas(int) sound_mode {
         u32 reverb : 7{};
         bool use_reverb : 1{};
@@ -59,22 +117,61 @@ namespace {
         u32 num_bits : 4{9};
     };
 
+    /**
+     * @brief Sets the sound driver mode based on the provided sound mode.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-1bh-gba---sounddrivermode">SWI 1Bh (GBA) - SoundDriverMode</a>
+     *
+     * \param mode Pointer to the sound mode structure.
+     *
+     * @sa sound_mode
+     */
     [[gnu::always_inline]]
     inline void SoundDriverMode(const sound_mode* mode) noexcept {
         register auto* r0 asm("r0") = mode;
         asm volatile inline ("swi 0x1B << ((1f - . == 4) * -16); 1:" : "+r"(r0) :: "r1", "r3", "memory");
     }
 
+    /**
+     * @brief Intended to be called in a VBlank interrupt handler.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-1dh-gba---sounddrivervsync">SWI 1Dh (GBA) - SoundDriverVSync</a>
+     *
+     * @warning The timing is critical, so make sure to call this first (highest priority) in your interrupt handler.
+     *
+     * @sa SoundDriverVSyncOff()
+     * @sa SoundDriverVSyncOn()
+     */
     [[gnu::always_inline]]
     inline void SoundDriverVSync() noexcept {
         asm volatile inline ("swi 0x1D << ((1f - . == 4) * -16); 1:" ::: "r0", "r1", "r3");
     }
 
+    /**
+     * @brief Disables the SoundDriverVSync() handler.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-28h-gba---sounddrivervsyncoff">SWI 28h (GBA) - SoundDriverVSyncOff</a>
+     *
+     * @warning If VBlank interrupts are stopped for whatever reason then this function must be called to protect the
+     *          sound driver until VBlank interrupts are re-enabled.
+     *
+     * @sa SoundDriverVSync()
+     * @sa SoundDriverVSyncOn()
+     */
     [[gnu::always_inline]]
     inline void SoundDriverVSyncOff() noexcept {
         asm volatile inline ("swi 0x28 << ((1f - . == 4) * -16); 1:" ::: "r0", "r1", "r3");
     }
 
+    /**
+     * @brief Enables the SoundDriverVSync() handler.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#swi-29h-gba---sounddrivervsyncon">SWI 29h (GBA) - SoundDriverVSyncOn</a>
+     *
+     * Corresponding function to SoundDriverVSyncOff() for when VSync interrupts are re-enabled.
+     *
+     * @warning After re-enabling VBlank interrupt and calling this function the SoundDriverVSync() must trigger via
+     *          VBlank interrupt ASAP.
+     *
+     * @sa SoundDriverVSync()
+     * @sa SoundDriverVSyncOff()
+     */
     [[gnu::always_inline]]
     inline void SoundDriverVSyncOn() noexcept {
         asm volatile inline ("swi 0x29 << ((1f - . == 4) * -16); 1:" ::: "r0", "r1", "r3");

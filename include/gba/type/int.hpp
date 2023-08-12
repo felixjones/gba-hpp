30fcc8f6a243842bf411dc348d41b226b5efcc18
Felix Jones <felix@felixjones.co.uk>
2023-08-13 01:39:12 +0200

    Documentation for integer type helpers.

--- include/gba/type/int.hpp
+++ include/gba/type/int.hpp
@@ -9,12 +9,18 @@
 
 #ifndef GBAXX_TYPE_INT_HPP
 #define GBAXX_TYPE_INT_HPP
+/** @file */
 
 #include <cstdint>
 #include <type_traits>
 
 namespace gba {
 
+    /**
+     * @brief Signed integer of a given minimum bit-length.
+     *
+     * @tparam N Minimum bit-length of the desired signed integer type.
+     */
     template <std::size_t N>
     using inttype = std::conditional_t<N <= 64 && N % 8 == 0,
                 std::conditional_t<N == 8,
@@ -31,12 +37,25 @@ namespace gba {
                     >
                 >, void>;
 
+    /**
+     * @brief Unsigned integer of a given minimum bit-length.
+     *
+     * @tparam N Minimum bit-length of the desired unsigned integer type.
+     *
+     * @sa inttype
+     */
     template <std::size_t N>
     using uinttype = std::make_unsigned_t<inttype<N>>;
 
+    /**
+     * @struct u4x2
+     * @brief 8-bit data type of two unsigned nibbles.
+     *
+     * @sa tile4bpp
+     */
     struct alignas(uinttype<8>) u4x2 {
-        uinttype<8> lo : 4{};
-        uinttype<8> hi : 4{};
+        uinttype<8> lo : 4{}; /**< The lower nibble bits (0x0F). */
+        uinttype<8> hi : 4{}; /**< The upper nibble bits (0xF0). */
     };
 
 } // namespace gba

e34260e0ceac1b7d4af53dc5ba2a664c0bd349de
Felix Jones <felix@felixjones.co.uk>
2023-08-13 22:05:44 +0200

    Documentation for 8x8 tile structs.

--- include/gba/type/tile.hpp
+++ include/gba/type/tile.hpp
@@ -9,15 +9,24 @@
 
 #ifndef GBAXX_TYPE_TILE_HPP
 #define GBAXX_TYPE_TILE_HPP
+/** @file */
 
 #include <gba/type/int.hpp>
 
 namespace gba {
 
+    /**
+     * @struct tile4bpp
+     * @brief 4bpp 8x8 tile.
+     */
     struct tile4bpp {
         u4x2 data[32];
     };
 
+    /**
+     * @struct tile8bpp
+     * @brief 8bpp 8x8 tile.
+     */
     struct tile8bpp {
         uinttype<8> data[64];
     };

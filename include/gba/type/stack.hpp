231b87617fa0bfc463f307a25441106073f5caf7
Felix Jones <felix@felixjones.co.uk>
2023-08-13 22:03:38 +0200

    Documentation for stack utilities functions.

--- include/gba/type/stack.hpp
+++ include/gba/type/stack.hpp
@@ -9,6 +9,7 @@
 
 #ifndef GBAXX_TYPE_STACK_HPP
 #define GBAXX_TYPE_STACK_HPP
+/** @file */
 
 #include <concepts>
 #include <cstdint>
@@ -17,11 +18,23 @@
 
 namespace gba::stack {
 
+    /**
+     * @brief Concept for checking if a type returns a pointer with the std::end() function.
+     *
+     * @tparam T The type to be checked.
+     */
     template <typename T>
     concept PointerEnd = requires(T a) {
         { std::end(a) } -> std::same_as<std::remove_cvref_t<decltype(a[0])>*>;
     };
 
+    /**
+     * @brief Reserve stack space for a type on the given stack.
+     *
+     * @tparam T Type to reserve allocation space for on the stack.
+     * @param stack A pointer to the top of the stack to be reserved.
+     * @return Pointer to the space reserved for type T on the stack.
+     */
     template <typename T>
     auto* reserve(void* stack) noexcept {
         static constexpr auto aligned = ((sizeof(T) + 7u) >> 3u) << 3u;
@@ -30,6 +43,15 @@ namespace gba::stack {
         return reinterpret_cast<T*>(pointer);
     }
 
+    /**
+     * @brief Reserve stack space for, and construct, a type on the given stack.
+     *
+     * @tparam T Type to reserve allocation space for & construct on the stack.
+     * @tparam Args Types for the arguments.
+     * @param stack A pointer to the top of the stack to emplace the type onto.
+     * @param args List of arguments to be forwarded to the type constructor.
+     * @return Pointer to the constructed type on the stack.
+     */
     template <typename T, typename... Args>
     auto* emplace(void* stack, Args&&... args) noexcept {
         static constexpr auto aligned = ((sizeof(T) + 7u) >> 3u) << 3u;
@@ -38,6 +60,16 @@ namespace gba::stack {
         return new(reinterpret_cast<void*>(pointer)) T(std::forward<Args>(args)...);
     }
 
+    /**
+     * @brief Pushes elements onto the stack.
+     *
+     * This function is used to add elements onto the stack. It takes any number of elements as parameters and adds them
+     * to the stack in the order they are passed.
+     *
+     * @param stack A pointer to the stack object.
+     * @param first The first element to be added to the stack.
+     * @param rest Any additional elements to be added to the stack.
+     */
     template <typename T, typename... Ts>
     auto* push(void* stack, T&& first, Ts&&... rest) noexcept {
         static constexpr auto aligned = ((sizeof(T) + 7u) >> 3u) << 3u;

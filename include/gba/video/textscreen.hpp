73ce6a4918cd8aa0bea0b17fe4f8c57bb4758d06
Felix Jones <felix@felixjones.co.uk>
2023-08-15 20:21:32 +0200

    Documentation for textscreen.

--- include/gba/video/textscreen.hpp
+++ include/gba/video/textscreen.hpp
@@ -9,16 +9,24 @@
 
 #ifndef GBAXX_VIDEO_TEXTSCREEN_HPP
 #define GBAXX_VIDEO_TEXTSCREEN_HPP
+/** @file */
 
 #include <gba/type.hpp>
 
 namespace gba {
 
+    /**
+     * @struct textscreen
+     * @brief Tile parameters for screen entries.
+     * @see <a href="https://mgba-emu.github.io/gbatek/#text-bg-screen-2-bytes-per-entry">Text BG Screen (2 bytes per entry)</a>
+     *
+     * @sa mmio::TEXT_SCREENBLOCKS
+     */
     struct alignas(short) textscreen {
-        u16 tile : 10 {};
-        bool hflip : 1 {};
-        bool vflip : 1 {};
-        u16 palbank : 4 {};
+        u16 tile : 10{}; /**< Which character graphic to use for this entry. This is relative to the character block for the background this screen belongs to. @sa bgcnt::charblock */
+        bool hflip : 1{}; /**< Should this tile be flipped horizontally. */
+        bool vflip : 1{}; /**< Should this tile be flipped vertically. */
+        u16 palbank : 4{}; /**< Which 16-color palette bank entry should be used for this tile. @sa mmio::BG_PALETTE */
     };
 
 } // namespace gba
